ebc5861e69d5893c7f248a312b1d191ceed5e832
jimbrian <ui.ux.jimbrian@gmail.com>
2022-11-20 10:07:52 +0300

    rails routes params practice

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,11 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  
+  #app/controllers/posts_controller.rb
 
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
 end
